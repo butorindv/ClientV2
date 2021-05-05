ddb828780bd99ab13b693145552b8f2719cafd99
Dmitriy <butorin_dv@mail.ru>
2021-05-05 13:10:32 +0300

    1

--- src/main/java/ru/butorin/MainClient.java
+++ src/main/java/ru/butorin/MainClient.java
@@ -1,7 +1,9 @@
 package ru.butorin;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class MainClient {
 
     public static void main(String[] args) {
